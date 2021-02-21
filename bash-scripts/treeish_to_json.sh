58656f8386da083d798e87a981bcbd2d60178ad5
Joe Doiron <jdoiron2@unum.com>
2021-02-21 09:51:05 -0500

    updated the format of the json

--- bash-scripts/treeish_to_json.sh
+++ bash-scripts/treeish_to_json.sh
@@ -6,15 +6,13 @@ cd "$1" # change to the .git directory
 
 # tree_objects is an array (sep by space) that stores each line, which is either a tree or blob
 mapfile -t tree_objects < <(git ls-tree -r -t $2)
-echo '{"objects": [' > "${output_file}"
+echo '{"objects": {' > "${output_file}"
 for (( i=0; i<${#tree_objects[@]}; i++ )) # for each object
     do
         line=( ${tree_objects[i]} )
-        dir=${line[@]:3} # get the last values in the array, which is the path (with spaces)
-        parent="${dir%/*}" # use bash string formatting to get the parent path
-        parent="${parent##*/}" # ./some/path/tp/parent -> parent
-        current_dir=${dir##*/}
-        echo "{\""perm\"": \""${line[0]}\"", \""type\"": \""${line[1]}\"", \""hash\"": \""${line[2]}\"", \""parent\"": \""$parent\"", \""name\"": \""$current_dir\""}," >> "${output_file}"
+        object_name=${line[@]:3} # e.g. parent_of_parent/parent/child
+        parent_name="${object_name%/*}" # e.g. parent_of_parent/parent
+        echo "\""$object_name\"": {\""perm\"": \""${line[0]}\"", \""type\"": \""${line[1]}\"", \""hash\"": \""${line[2]}\"", \""parent\"": \""$parent_name\"", \""id\"": \""$object_name\""}," >> "${output_file}"
     done
-echo '{"empty": 0}]}' >> "${output_file}"
+echo '"empty": 0}}' >> "${output_file}"
 
