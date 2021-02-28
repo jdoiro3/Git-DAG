cb4e1330c9a3ccd033a25af41490f4b6f17d8e9e
Joseph Doiron <josephdoiron1234@yahoo.com>
2021-02-28 10:24:53 -0500

    using shell recursion so parents are hashes instead of paths

--- bash-scripts/treeish_to_json.sh
+++ bash-scripts/treeish_to_json.sh
@@ -4,15 +4,27 @@ cd ../ # change to the project's root directory
 output_file="$PWD/data/tree.json"
 cd "$1" # change to the .git directory
 
-# tree_objects is an array (sep by space) that stores each line, which is either a tree or blob
-mapfile -t tree_objects < <(git ls-tree -r -t $2)
-echo '{"objects": {' > "${output_file}"
-for (( i=0; i<${#tree_objects[@]}; i++ )) # for each object
-    do
+tree_object_to_json()
+{
+    mapfile -t tree_objects < <(git ls-tree $1)
+
+    for (( i=0; i<${#tree_objects[@]}; i++ )) do # for each object
+
         line=( ${tree_objects[i]} )
         object_name=${line[@]:3} # e.g. parent_of_parent/parent/child
-        parent_name="${object_name%/*}" # e.g. parent_of_parent/parent
-        echo "\""$object_name\"": {\""perm\"": \""${line[0]}\"", \""type\"": \""${line[1]}\"", \""hash\"": \""${line[2]}\"", \""parent\"": \""$parent_name\"", \""id\"": \""$object_name\""}," >> "${output_file}"
+
+        if [ "${line[1]}" == "tree" ]; then
+            echo "\""${line[2]}\"": {\""perm\"": \""${line[0]}\"", \""type\"": \""${line[1]}\"", \""parents\"": \""$1\"", \""name\"": \""$object_name\"", \""id\"": \""${line[2]}\""}," >> "${output_file}"
+            unset $1
+            (tree_object_to_json ${line[2]}) # subshell
+        else
+            echo "\""${line[2]}\"": {\""perm\"": \""${line[0]}\"", \""type\"": \""${line[1]}\"", \""parents\"": \""$1\"", \""name\"": \""$object_name\"", \""id\"": \""${line[2]}\""}," >> "${output_file}"
+        fi
+
     done
+}
+
+echo '{"objects": {' > "${output_file}"
+tree_object_to_json $2
 echo '"empty": 0}}' >> "${output_file}"
 
